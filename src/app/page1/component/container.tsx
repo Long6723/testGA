b177b02c7f2d10b4ad23b334312e8dfeb404a959
luulong <long06072003@gmail.com>
2025-10-17 21:03:57 +0700

    fix: test GA

--- src/app/page1/component/container.tsx
+++ src/app/page1/component/container.tsx
@@ -3,8 +3,16 @@
 import Link from "next/link";
 
 export default function PageT1() {
-  const handleClick = () => {
+  const handleClick = async () => {
     console.log("Hello from Page 1");
+
+    try {
+      if ((window as any)?.gtag) {
+        await (window as any)?.gtag("event", "button_click");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
--- src/app/page2/component/container.tsx
+++ src/app/page2/component/container.tsx
@@ -8,7 +8,32 @@ export default function PageT2() {
 
     try {
       if ((window as any)?.gtag) {
-        await (window as any)?.gtag("event", "button_click", "page2_button");
+        await (window as any)?.gtag("event", "add_to_cart", {
+          currency: "USD",
+          value: 30.03,
+          items: [
+            {
+              item_id: "SKU_12345",
+              item_name: "Stan and Friends Tee",
+              affiliation: "Google Merchandise Store",
+              coupon: "SUMMER_FUN",
+              discount: 2.22,
+              index: 0,
+              item_brand: "Google",
+              item_category: "Apparel",
+              item_category2: "Adult",
+              item_category3: "Shirts",
+              item_category4: "Crew",
+              item_category5: "Short sleeve",
+              item_list_id: "related_products",
+              item_list_name: "Related Products",
+              item_variant: "green",
+              location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+              price: 10.01,
+              quantity: 3,
+            },
+          ],
+        });
       }
     } catch (error) {
       console.log(error);
