dea52976a2e7b0c63c77447617364d54b401e108
luulong <long06072003@gmail.com>
2025-10-20 12:05:24 +0700

    feat: robots.ts and sitemap.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api", "/admin"],
+        disallow: "",
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
